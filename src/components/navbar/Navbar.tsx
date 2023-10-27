404171e680d0ec846b4b5b01cb9a1d1b58aba12f
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-10-27 17:36:56 -0300

    Refactor: Renaming files

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,15 @@
-import { useState } from 'react';
-import { createPortal } from 'react-dom';
-import { MenuIcon } from '@/utils/Icons';
-import NavbarMenu from './navbar-menu/NavbarMenu';
+import Logo from '../logo/Logo';
+import SettingsButton from './settings-button/SettingsButton';
+import SidebarButton from './sidebar-button/SidebarButton';
 
 export default function Navbar() {
-  const [toggle, setToggle] = useState<boolean>(false);
   return (
-    <>
-      <div className="relative z-40 flex h-14 w-screen items-center justify-between bg-dark-blue-80 px-4 shadow-sm shadow-transparent-black">
-        <button
-          className="flex h-auto w-auto items-center justify-center"
-          onClick={() => setToggle(!toggle)}
-        >
-          <MenuIcon />
-        </button>
+    <div className="relative z-40 flex h-14 w-screen items-center justify-between bg-dark-blue-80 px-4 shadow-sm shadow-transparent-black">
+      <div className="flex items-center gap-4">
+        <SidebarButton />
+        <Logo />
       </div>
-      {createPortal(
-        <NavbarMenu toggle={toggle} onClose={() => setToggle(false)}></NavbarMenu>,
-        document.getElementById('portal-root')!,
-      )}
-    </>
+      <SettingsButton></SettingsButton>
+    </div>
   );
 }
---
+++ src/components/navbar/sidebar/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { XIcon } from '@/utils/Icons';
+
+interface SidebarProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function Sidebar({ isOpen, onClose }: SidebarProps) {
+  return (
+    <>
+      <div
+        className={`${
+          isOpen ? `flex` : `hidden`
+        } absolute top-0 z-[41] h-screen w-screen bg-opacity-0`}
+        onClick={onClose}
+      ></div>
+      <div
+        className={`${
+          isOpen ? `translate-x-0` : `-translate-x-[400px]`
+        } absolute top-0 z-[42] flex h-screen w-[400px] transform-gpu flex-col bg-dark-blue-80 shadow-md shadow-transparent-black duration-200 ease-in-out`}
+      >
+        <div className="flex justify-end p-4">
+          <button onClick={onClose} className="hover:animate-pulse">
+            <XIcon></XIcon>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}

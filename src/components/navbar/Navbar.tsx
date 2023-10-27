dee7efdc90ded0c3a8a4f4cf99e32b5d15050ec0
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-10-27 15:41:10 -0300

    Bootstrap: Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,24 @@
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import { MenuIcon } from '@/utils/Icons';
+import NavbarMenu from './navbar-menu/NavbarMenu';
+
 export default function Navbar() {
-  return <div className="w-screen bg-dark-blue-80 h-14 z-14 items-center justify-between"></div>;
+  const [toggle, setToggle] = useState<boolean>(false);
+  return (
+    <>
+      <div className="relative z-40 flex h-14 w-screen items-center justify-between bg-dark-blue-80 px-4 shadow-sm shadow-transparent-black">
+        <button
+          className="flex h-auto w-auto items-center justify-center"
+          onClick={() => setToggle(!toggle)}
+        >
+          <MenuIcon />
+        </button>
+      </div>
+      {createPortal(
+        <NavbarMenu toggle={toggle} onClose={() => setToggle(false)}></NavbarMenu>,
+        document.getElementById('portal-root')!,
+      )}
+    </>
+  );
 }
--- src/components/navbar/navbar-menu/NavbarMenu.tsx
+++ src/components/navbar/navbar-menu/NavbarMenu.tsx
@@ -1,3 +1,22 @@
-export default function Sidebar() {
-  return <div></div>;
+interface NavbarMenuProps {
+  toggle: boolean;
+  onClose: () => void;
+}
+
+export default function NavbarMenu({ toggle, onClose }: NavbarMenuProps) {
+  return (
+    <>
+      <div
+        className={`${
+          toggle ? `flex` : `hidden`
+        } absolute top-0 z-[41] h-screen w-screen bg-opacity-0`}
+        onClick={onClose}
+      ></div>
+      <div
+        className={`${
+          toggle ? `translate-x-0` : `-translate-x-[400px]`
+        } absolute top-0 z-[42] flex h-screen w-[400px] transform-gpu bg-dark-blue-80 shadow-md shadow-transparent-black duration-200 ease-in-out`}
+      ></div>
+    </>
+  );
 }

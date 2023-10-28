f0b7efc849f4387d48c00c39dc64fac80ce274f9
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-10-28 10:49:01 -0300

    Bootstrap: Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,24 @@
 export default function Home() {
-  return <div>THIS IS HOME</div>;
+  return (
+    <div className="flex h-full w-screen flex-col overflow-y-auto bg-grey-5 dark:bg-gocn-grey-primary dark:text-white">
+      <div className="flex w-screen bg-white p-4 shadow-sm shadow-transparent-black dark:bg-grey-80">
+        <div>
+          <h1 className="text-2xl font-bold">Home Page</h1>
+          <h2 className="text-xl dark:text-grey-20">
+            This page is a mere prototype of a portal to present a company's resources.
+          </h2>
+        </div>
+      </div>
+      <div className="flex h-full w-screen items-start justify-center px-4 py-20">
+        <div className="flex flex-wrap justify-center gap-4">
+          <div className="h-56 w-96 rounded-md bg-white shadow-sm shadow-transparent-black dark:bg-grey-80"></div>
+          <div className="h-56 w-96 rounded-md bg-white shadow-sm shadow-transparent-black dark:bg-grey-80"></div>
+          <div className="h-56 w-96 rounded-md bg-white shadow-sm shadow-transparent-black dark:bg-grey-80"></div>
+          <div className="h-56 w-96 rounded-md bg-white shadow-sm shadow-transparent-black dark:bg-grey-80"></div>
+          <div className="h-56 w-96 rounded-md bg-white shadow-sm shadow-transparent-black dark:bg-grey-80"></div>
+          <div className="h-56 w-96 rounded-md bg-white shadow-sm shadow-transparent-black dark:bg-grey-80"></div>
+        </div>
+      </div>
+    </div>
+  );
 }

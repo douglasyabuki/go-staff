f33607fafb603169b039e6b1245957669d98832e
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-10-30 14:13:15 -0300

    Bootstrap: Signout button

--- src/components/navbar/sidebar/Sidebar.tsx
+++ src/components/navbar/sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
-import { XIcon } from '@/utils/Icons';
+import { AuthContext } from '@/contexts/AuthContext';
+import { ExitIcon, XIcon } from '@/utils/Icons';
+import { useContext } from 'react';
 
 interface SidebarProps {
   isOpen: boolean;
@@ -6,6 +8,7 @@ interface SidebarProps {
 }
 
 export default function Sidebar({ isOpen, onClose }: SidebarProps) {
+  const { signOut } = useContext(AuthContext);
   return (
     <>
       <div
@@ -17,11 +20,21 @@ export default function Sidebar({ isOpen, onClose }: SidebarProps) {
       <div
         className={`${
           isOpen ? `translate-x-0` : `-translate-x-[400px]`
-        } absolute top-0 z-[42] flex h-screen w-[400px] transform-gpu flex-col bg-dark-blue-80 shadow-md shadow-transparent-black duration-200 ease-in-out dark:bg-gocn-grey-primary`}
+        } absolute top-0 z-[42] flex h-screen w-[400px] transform-gpu flex-col divide-y-[0.5px] divide-grey-20 bg-dark-blue-80 shadow-md shadow-transparent-black duration-200 ease-in-out dark:bg-gocn-grey-primary`}
       >
-        <div className="flex justify-end p-4">
+        <div className="flex h-[15%] items-start justify-end p-4">
           <button onClick={onClose} className="hover:animate-pulse">
             <XIcon></XIcon>
+          </button>
+        </div>
+        <div className="h-[75%]"></div>
+        <div className="flex h-[10%] items-center justify-end p-4">
+          <button
+            onClick={signOut}
+            className="group flex items-center gap-2 font-semibold text-grey-20 transition-colors duration-200 hover:text-white"
+          >
+            <ExitIcon />
+            Sign Out
           </button>
         </div>
       </div>
